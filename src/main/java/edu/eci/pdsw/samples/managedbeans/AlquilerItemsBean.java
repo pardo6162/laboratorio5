0977a56620dc6400bd54da13ad40a805f353a3ec
pardo6162 <a.pardo6162@gmail.com>
2017-03-09 00:06:20 -0500

    Pagina de registro  incompleta

--- src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
@@ -22,21 +22,65 @@ import javax.faces.bean.SessionScoped;
 public class AlquilerItemsBean implements Serializable {
 
     ServiciosAlquiler sp = ServiciosAlquiler.getInstance();
-    
+    String nnombre=null;
+    long nidentificacion=0 ;
+    String ntelefono=null;
+    String ndireccion=null;
+    String nemail=null;
+    boolean error=false;
     public AlquilerItemsBean() {
         
+    }
+    public String getNnombre(){
+        return  nnombre;
+    }
+    public void setNnombre(String nombre){
+        nnombre=nombre;
+    }
+    public long getNidentificacion(){
+        return  nidentificacion;
+    }
+    public void setNidentificacion(long id){
+        nidentificacion=id;
+    }
+    public String getNtelefono(){
+        return  ntelefono;
+    }
+    public void setNtelefono(String tel){
+        ntelefono=tel;
+    }
+    public String getNdireccion(){
+        return  ndireccion;
+    }
+    public void setNdireccion(String dir){
+        ndireccion=dir;
+    }
+    public String getNemail(){
+        return  nemail;
+    }
+    public void setNemail(String mail){
+        nemail=mail;
     }
     public List<Cliente> getClientes() {
         List<Cliente> clientes= new ArrayList<Cliente>();
         try{
             clientes = sp.consultarClientes();
         }catch(Exception e){
-            
+           error=true; 
         }
         return  clientes;
     }
-    public void setClientes(Cliente nCLient){
-        
+    public void registrarCliente(){
+        try{
+            sp.registrarCliente(new Cliente(nnombre,nidentificacion,ntelefono,ndireccion,nemail));
+            String nnombre=null;
+            nidentificacion=0 ;
+            ntelefono=null;
+            ndireccion=null;
+            nemail=null;
+        }catch(Exception e){
+            error=true;
+        }
     }
 
 }
