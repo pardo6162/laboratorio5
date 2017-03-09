a414469c162907b9fb06b0d6f3314afd8da7d982
pardo6162 <a.pardo6162@gmail.com>
2017-03-08 21:44:57 -0500

    Avance en RegistroCLientes.xhtml metodo clientes en el bean

--- src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
@@ -5,8 +5,11 @@
  */
 package edu.eci.pdsw.samples.managedbeans;
 
+import edu.eci.pdsw.samples.entities.Cliente;
 import edu.eci.pdsw.samples.services.ServiciosAlquiler;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -19,8 +22,21 @@ import javax.faces.bean.SessionScoped;
 public class AlquilerItemsBean implements Serializable {
 
     ServiciosAlquiler sp = ServiciosAlquiler.getInstance();
-
+    
     public AlquilerItemsBean() {
+        
+    }
+    public List<Cliente> getClientes() {
+        List<Cliente> clientes= new ArrayList<Cliente>();
+        try{
+            clientes = sp.consultarClientes();
+        }catch(Exception e){
+            
+        }
+        return  clientes;
+    }
+    public void setClientes(Cliente nCLient){
+        
     }
 
 }
